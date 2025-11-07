a92963c49e9006c6a7f6592c5637622ff40fcb8a
krishaggarwal088 <krishagg088@gmail.com>
2025-11-07 20:37:32 +0530

    Methods and loops

---
+++ lesson 20/script.js
@@ -0,0 +1,6 @@
+for (i = 1; i < 11; i++) {
+number + i + "<br>";
+}
+document.write("The random value is"+Math.floor(Math.random() * 100));
+var date =new Date();
+document.write("Today's Date is" + date.getDate())
