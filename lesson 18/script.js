18fb9a32814cba2a515f989f24ac6a0143d4b2c9
krishaggarwal088 <krishagg088@gmail.com>
2025-11-21 19:17:47 +0530

    print random number

--- lesson 18/script.js
+++ lesson 18/script.js
@@ -1,9 +1,9 @@
-var num1 = 10;
-var num2 = 2;
-var add num1 +num2;
-var sub = num1 -num2;
-var mul num1 *num2;
-var div num1 /num2;
+var num1 = 21;
+var num2 = 10;
+var add=num1+num2;
+var sub=num1-num2;
+var mul=num1*num2;
+var div=num1/num2;
 document.getElementById("add").innerHTML =
 "The added value is: " + add;
 document.getElementById("sub").innerHTML =
---
+++ lesson 24/script.js
@@ -0,0 +1,12 @@
+var i; 
+var text = " ";
+{
+let i = 0; 
+while (i<10){
+text += "The values inside the block: " + i + "</br>";
+i++;
+}   
+document.getElementById("result").innerHTML = text;
+}
+document.write("The value of i outside the block is:"
++5);
