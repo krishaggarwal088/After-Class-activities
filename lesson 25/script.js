502e49ff39badf75bd4661d35691c211f63a4577
krishaggarwal088 <krishagg088@gmail.com>
2025-12-29 17:07:32 +0530

    Class in JS

---
+++ lesson 25/script.js
@@ -0,0 +1,27 @@
+class Student {
+constructor(name) {
+this.studentName = name;
+}
+static hello(){
+return "Hello";
+}
+present() {
+return 'My name is '
++ this.studentName
+}
+}
+class Grade extends Student {
+constructor (name, grade) {
+super(name);
+this.studentGrade = grade;
+}
+show() {
+return this.present() + from
+this.studentGrade;
+}   
+}
+let myStudent = new Grade("Tina", "grade 6");
+document.getElementById("details").innerHTML =
+myStudent.show();
+document.getElementById("greetings").innerHTML =
+Student.hello();    
