595f6b72b7212a1dc1ab514d7a906b175d5db96d
krishaggarwal088 <krishagg088@gmail.com>
2026-01-23 16:45:30 +0530

    asynchronus functions in js

---
+++ lesson 28/script.js
@@ -0,0 +1,9 @@
+async function product(a, b) {
+    let response = await a * b;
+    display(response);
+}
+
+function display(some) {
+    document.getElementById("result").innerHTML = some;
+}
+product(5,20);
